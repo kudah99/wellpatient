eb9fd1887f2cdbe39036b93f9f18384a3a1da4c8
Kudakwashe Chris Chipangura <kcchipangura>
2025-03-31 14:13:37 +0200

    Login Page fixing

--- well_patient/well_patient/urls.py
+++ well_patient/well_patient/urls.py
@@ -4,7 +4,16 @@ URL configuration for well_patient project.
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+     path("", RedirectView.as_view(url="/admin/", permanent=False)),
     path('admin/', admin.site.urls)
 ]
+
+# Serve static and media files during development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
